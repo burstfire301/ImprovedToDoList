63c1e14ca16af898c8a5f628144bc8f2a10bf8ca
joneschri8 <joneschri8@ecs.vuw.ac.nz>
2024-02-19 21:50:55 +1300

    improved adding to list

--- Main.js
+++ Main.js
@@ -84,32 +84,10 @@ function addItemToList(value) {
 
 // Create a new list item when clicking on the "Add" button
 function newElementFromAdd() {
-  // create a new list item
-  var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
 
-  // create a close button
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-
-  // add functionality to close button
-  span.onclick = function () {
-    var div = this.parentElement;
-    div.style.display = "none";
-  };
-
-  // add close button to list item
-  li.appendChild(span);
-
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    document.getElementById("routine-list").appendChild(li);
+  if (inputValue !== "") {
+    addItemToList(inputValue);
+    document.getElementById("myInput").value = "";
   }
-
-  document.getElementById("myInput").value = "";
 }
